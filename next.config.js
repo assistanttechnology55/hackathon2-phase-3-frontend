e5e3f2f97e4481be0764249314dd4cefacabe017
assistanttechnology55 <um2030155@gmail.com>
2026-03-07 02:30:25 +0500

    Update remotePatterns for image configuration

--- next.config.js
+++ next.config.js
@@ -2,20 +2,25 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
-        pathname: '/**',
-      },
-      {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '8000',
-        pathname: '/**',
+ {
+     protocol: 'http',
+     hostname: 'localhost',
+     port: '8000',
+    pathname: '/**',
+  },
+{
+   protocol: 'http',
+   hostname: '127.0.0.1',
+   port: '8000',
+   pathname: '/**',
+  }, 
+{
+  protocol: 'https',
+  hostname:
+ 'hackathon-phase3-backend-production.up.railway.app',
+  pathname: '/**',
       },
     ],
   },
-};
-
+ };
 module.exports = nextConfig;
